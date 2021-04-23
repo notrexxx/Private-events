c98a11e7348463f76bf2167cae2be16524b074e4
notrexxx <notrexx15@gmail.com>
2021-04-22 22:07:00 -0400

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root 'users#new'
+  resources :users
+  resources :events, only: %i[show new create index]
+  resources :sessions
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
-  resources :users, :sessions
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
