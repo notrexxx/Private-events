6dc28b40f2bdd86bc3418c59e061765a428079b6
notrexxx <notrexx15@gmail.com>
2021-04-23 19:36:16 -0400

    Add navigation and helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+
+  def logout_link
+    if logged_in?
+    (link_to 'Logout', logout_path, class: 'btn btn-outline-danger')
+
+    end
+  end
+
+  def nav_link_events
+    if logged_in?
+      link_to 'My Events',current_user, class: 'btn btn-outline-primary'
+    end
+  end
+
+  def nav_link_new_event
+    if logged_in?
+      link_to 'Create Event',  new_event_path, class: 'btn btn-outline-success'
+    end
+  end
+
+  def login_link
+    (link_to 'Login', login_path, class: 'btn btn-outline-success') unless logged_in?
+  end
+  def signup_link
+    (link_to 'Signup', signup_path, class: 'btn btn-outline-primary') unless logged_in?
+  end
+
 end
