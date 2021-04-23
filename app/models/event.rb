a99b1e99fe202942fc354192b94ebc250fc87a7a
notrexxx <notrexx15@gmail.com>
2021-04-23 13:34:20 -0400

    Add associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: "user_id"
   validates :date, :name, :description, presence: true
+  has_many :registrations
+  has_many :attendees, through: :registrations, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 15 }
   has_many :events, inverse_of: :creator
+  has_many :registrations
+  has_many :attended_events, through: :registrations, source: :event
 end
