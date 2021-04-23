22a36090fc17d974bd7a5db317db835f66ba4065
notrexxx <notrexx15@gmail.com>
2021-04-23 19:37:27 -0400

    Update views to use bootstrap

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,6 @@ class Event < ApplicationRecord
   validates :date, :name, :description, presence: true
   has_many :registrations
   has_many :attendees, through: :registrations, source: :user
+  scope :future, -> { where('date >= ?', Time.now) }
+  scope :past_event, -> { where('date <= ?', Time.now) }
 end
